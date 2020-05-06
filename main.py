f4c09fba9779928f6ddc74cd6f9fb57277763a99
hhq <kirsch0421@hotmail.com>
2020-05-06 16:49:48 -0500

    Updates

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-from flask import Flask 
-  
+from flask import Flask, request, jsonify, Response, render_template
+import pandas as pd
+import numpy as np
+
+
+print('running')
+
 app = Flask(__name__) 
   
 @app.route("/") 
 def home_view(): 
         return "<h1>test</h1>"
+        
+if __name__ == "__main__":
+    app.run(debug=True)
